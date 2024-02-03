33d1bdde86734a3f55d5537c72ad28dcb97a6506
vinithuday <vinithrajbmu@gmail.com>
2024-02-03 16:59:00 +0100

    cors removed

--- smartparking-backend/server.js
+++ smartparking-backend/server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const User = require('./models/User');
 const authRoutes = require('./routes/authRoutes');
-const cors = require('cors'); 
+// const cors = require('cors'); 
 
 
 const app = express();
@@ -43,7 +43,7 @@ run().catch(console.dir);
 
 app.use(express.json());
 
-app.use(cors());
+// app.use(cors());
 
 
 app.use('/api/auth', authRoutes);
