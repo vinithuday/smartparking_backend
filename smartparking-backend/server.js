e9cb2938a0ca81e7419d04b29281616912eb1ff7
vinithuday <vinithrajbmu@gmail.com>
2024-02-11 13:27:24 +0100

    updated server.js and allRoutes

--- smartparking-backend/server.js
+++ smartparking-backend/server.js
@@ -44,33 +44,33 @@ app.post('/payment-sheet', async (req, res) => {
   });
 });
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://vinithrajbmu:mNOJUT3pyR2o0klU@cluster0.sginhlx.mongodb.net/?retryWrites=true&w=majority";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: false,
-    deprecationErrors: true,
-  }
-});
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://vinithrajbmu:7YIdkz09xSGsAyRo@cluster0.sginhlx.mongodb.net/"
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: false,
+//     deprecationErrors: true,
+//   }
+// });
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
 
-app.use(express.json());
+ app.use(express.json());
 
-// app.use(cors());
+app.use(cors());
 
 
 app.use('/api/auth', authRoutes);
