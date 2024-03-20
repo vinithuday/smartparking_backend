0b7e7d41695da4b094c42f54ad0e4977d7701b98
vinithuday <vinithrajbmu@gmail.com>
2024-03-20 21:37:27 +0100

    updated auth.js

--- smartparking-backend/routes/auth.js
+++ smartparking-backend/routes/auth.js
@@ -1,35 +1,38 @@
 const router = require("express").Router();
 const { User1, validate } = require("../models/user1");
 const bcrypt = require("bcrypt");
-const {connectToCollection,disconnectFromCollection} = require("../db");
+const { connectToCollection, disconnectFromCollection } = require("../db");
 const Joi = require("joi");
 
 router.post("/", async (req, res) => {
-	try {
-		const { error } = validate(req.body);
-		if (error)
-			return res.status(400).send({ message: error.details[0].message });
+  try {
+    const { error } = validate(req.body);
+    if (error)
+      return res.status(400).send({ message: error.details[0].message });
 
-		const user = await User1.findOne({ email: req.body.email });
-		if (!user){
-			return res.status(401).send({ message: "Invalid Email or Password" });
-		}
-			
+    const user = await User1.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(401).send({ message: "Invalid Email or Password" });
+    }
 
-		const validPassword = await bcrypt.compare(
-			req.body.password,
-			user.password
-		);
-		if (!validPassword)
-			return res.status(401).send({ message: "Invalid Email or Password" });
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword)
+      return res.status(401).send({ message: "Invalid Email or Password" });
 
-		const token = user.generateAuthToken();
-		res.status(200).send({ data: token, message: "logged in successfully" ,email:req.body.email});
-	    
-	} catch (error) {
-		res.status(500).send({ message: "Internal Server Error" });
-	}
+    const token = user.generateAuthToken();
+    res
+      .status(200)
+      .send({
+        data: token,
+        message: "logged in successfully",
+        email: req.body.email,
+      });
+  } catch (error) {
+    res.status(500).send({ message: "Internal Server Error" });
+  }
 });
-
 
 module.exports = router;
